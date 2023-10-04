9a1bb58caa7e8a8ca78f6594063e7dbb1c5820b3
Kyle Espinola <kyle.s.espinola@gmail.com>
2023-10-04 19:14:56 +0200

    feat: init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HUB Uploads
+
+A simple REST wrapper for web3up used as a shared uploads service within HUB.
+
+## Getting started
+
+```
+npm i
+npm run start
+```
---
+++ server.js
@@ -0,0 +1,31 @@
+import Fastify from "fastify";
+import MultiPart from "@fastify/multipart";
+import { Signer } from "@ucanto/principal/ed25519";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.register(MultiPart);
+
+fastify.get("/health", async function handler(request, reply) {
+  reply.send({ status: "ok" });
+});
+
+// Declare a route
+fastify.post("/uploads", async function handler(request, reply) {
+  const parts = req.files();
+
+  for await (const part of parts) {
+  }
+
+  reply.send();
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
